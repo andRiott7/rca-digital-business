02b29df2e32ddcbdbff12cb0c1d7df5a8b225c77
Thiago Andriotti <andriott7@gmail.com>
2021-04-12 10:42:33 -0300

    componente para itens de produto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product/product.model.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    id: number;
+    title: string;
+    category: string;
+    price: string;
+    imagePath: string;
+}
