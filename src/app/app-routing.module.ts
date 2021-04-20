b6bcca38df19d86c6be2ca07592133d4070119f9
Thiago Andriotti <andriott7@gmail.com>
2021-04-20 10:45:26 -0300

    atualização do readme.md.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component'
 import { ShoppingCartComponent } from './product/shopping-cart/shopping-cart.component'
 
 const routes: Routes = [
-    // { path: '', redirectTo: 'product' },
     { path: 'products', component: ProductComponent},
     { path: 'shopping-cart', component:  ShoppingCartComponent},
     { path: '**', redirectTo: 'products' }
